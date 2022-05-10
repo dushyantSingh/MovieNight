4e1faf3971202ab72b76357de095026507f48fe3
Dushyant Singh <dushyantsingh1097@gmail.com>
2022-05-10 10:28:52 +0800

    Added search button control view model event

--- MovieNight/MovieHome/MovieHomeViewController.swift
+++ MovieNight/MovieHome/MovieHomeViewController.swift
@@ -7,15 +7,19 @@
 
 import Foundation
 import UIKit
+import RxSwift
 
 class MovieHomeViewController: UIViewController {
     var viewModel: MovieHomeViewModel!
     @IBOutlet weak var movieSearchTextField: CustomTextField!
     @IBOutlet weak var searchButton: PrimaryButton!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+
+    private let disposeBag = DisposeBag()
     override func viewDidLoad() {
         super.viewDidLoad()
-        viewModel.searchMovies(searchText: "Marvel")
         setupView()
+        setupEvents()
     }
 }
 
@@ -24,5 +28,23 @@ private extension MovieHomeViewController {
         movieSearchTextField.title = "Movie name"
         movieSearchTextField.titleFontSize = .large
         searchButton.setTitle("Search", for: .normal)
+        searchButton.rx.tap.asObservable()
+            .map { [weak self] in
+                return self?.movieSearchTextField.text ?? ""
+            }
+            .filterEmpty()
+            .bind(to: viewModel.getMovies)
+            .disposed(by: disposeBag)
+    }
+    func setupEvents() {
+        viewModel.events.asObservable()
+            .subscribe(onNext: { [weak self] event in
+                if case .loading = event {
+                    self?.activityIndicator.startAnimating()
+                } else {
+                    self?.activityIndicator.stopAnimating()
+                }
+            })
+            .disposed(by: disposeBag)
     }
 }
--- MovieNight/MovieHome/MovieHomeViewModel.swift
+++ MovieNight/MovieHome/MovieHomeViewModel.swift
@@ -7,27 +7,48 @@
 
 import Foundation
 import RxSwift
+import RxCocoa
 
 struct MovieHomeViewModel {
+    enum Event {
+        case loading
+        case error(String)
+        case displayMovies([Movie])
+    }
+
     private let service: MovieServiceType
+    let getMovies = PublishSubject<String>()
+    let events = PublishSubject<Event>()
     var disposeBag = DisposeBag()
 
     init(service: MovieServiceType = ServiceFactory.movieService) {
         self.service = service
+        setupMovieSearch()
     }
+}
 
-    func searchMovies(searchText: String) {
-        service.retrieveMovieList(text: searchText)
-            .subscribe(onNext: { searchResult in
+private extension MovieHomeViewModel {
+    func setupMovieSearch() {
+        getMovies.asObservable()
+            .flatMap { service.retrieveMovieList(text: $0) }
+            .map { searchResult in
                 switch searchResult {
                 case .success(let response):
                     print(response)
+                    if let movieResult = response as? SearchResponse,
+                       let movies = movieResult.search {
+                        return .displayMovies(movies)
+                    } else {
+                        return .error("No movies found")
+                    }
                 case .failed:
                     print("Failed")
+                    return .error("Movie fetch failed")
                 case .waiting:
-                    print("Waiting")
+                    return .loading
                 }
-            })
+            }
+            .bind(to: events)
             .disposed(by: disposeBag)
     }
 }
